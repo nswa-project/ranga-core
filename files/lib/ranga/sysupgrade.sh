e861c65ed24af6930d9bb3be753eb85a59dc120a
nswa-project <41747840+nswa-project@users.noreply.github.com>
2020-05-03 17:10:07 +0800

    sysupgrade: mod for opensource version

--- files/lib/ranga/sysupgrade.sh
+++ files/lib/ranga/sysupgrade.sh
@@ -1,11 +1,5 @@
 #!/bin/sh
 
-key="09810395519ba0fe"
-
-denc() {
-	aescrypt2 1 "$1" "$2" "$key" >/dev/null 2>&1
-}
-
 _info() {
 	echo "[upgrade] $1" >> /tmp/sysupgrade.log
 }
@@ -18,43 +12,18 @@ errquit() {
 
 reboot="$1"
 
-_info "Decoding firmware..."
-denc /tmp/nswaup.bin /tmp/nswaup.tar
-rm -f /tmp/nswaup.bin
-
-_info "Parsing firmware metadata..."
-tar xvf /tmp/nswaup.tar -C /tmp > /dev/null
-rm -f /tmp/nswaup.tar
-
-[ -f "/tmp/nswaupd.tar.gz.sig" ] || errquit "firmware invalid"
-[ -f "/tmp/nswaupd.tar.gz" ] || errquit "firmware invalid"
-
-_info "Checking data integrity..."
-pk_verify /pubkey-trust /tmp/nswaupd.tar.gz > /dev/null 2>&1
-if [ "$?" != "0" ]; then
-	errquit "firmware verify failed"
-fi
-
 _info "Unpacking firmware..."
 rm -rf /tmp/nswafw/
-tar xzvf /tmp/nswaupd.tar.gz -C /tmp > /dev/null
+tar xzf /tmp/nswa-patch.tar.gz nswafw -C /tmp > /dev/null
 
 [ -d "/tmp/nswafw" ] || errquit "firmware invalid"
 
-rm -f /tmp/nswaupd.tar.gz.sig
-rm -f /tmp/nswaupd.tar.gz
 _info "Installing..."
 
-#if ! [ -f /tmp/nswafw/ranga_r2 ]; then
-#	errquit "This patch is NOT suitable for NSWA 4.x Ranga systems"
-#fi
-#if [ "`cat /tmp/nswafw/ranga_r2 2>/dev/null`" != "3" ]; then
-#	errquit "This patch is NOT suitable for current NSWA 4.x Ranga systems"
-#fi
-[ -f /tmp/nswafw/basevcn ] || errquit "This patch is NOT suitable for current NSWA Ranga systems"
-package_basevcn=`head -1 /tmp/nswafw/basevcn`
-best_basevcn='4'
-[ "$package_basevcn" -lt "$best_basevcn" ] && errquit "This patch is NOT suitable for current NSWA Ranga systems"
+[ -f /tmp/nswafw/basevcn-osv ] || errquit "This patch is NOT suitable for current NSWA Ranga systems (Open-source versions)"
+package_basevcn=`head -1 /tmp/nswafw/basevcn-osv`
+best_basevcn='1'
+[ "$package_basevcn" -lt "$best_basevcn" ] && errquit "This patch is NOT suitable for current NSWA Ranga systems (Open-source versions)"
 
 [ -f /tmp/nswafw/noreboot ] && reboot='0'
 
